4cbcad0fdbc03e84e4c90607050705376922c838
Blake La Pierre <blakelapierre@gmail.com>
2018-04-17 14:53:05 -0400

    latest

--- src/app.js
+++ src/app.js
@@ -1,85 +1,136 @@
 import { h, render } from 'preact-cycle';
 
 
-const markets = {
-  'tradogre': {},
-  'tradesatoshi': {
-    'orderbook': 'https://tradesatoshi.com/api/public/getorderbook?market=TRTL_BTC&type=both&depth=200'
-  }
-};
-
-
 const START = (_, mutation) => {
   _.started = true;
 
   LISTEN_TO_TRADEOGRE(_, mutation);
-  LISTEN_TO_TRADESATOSHI(_, mutation);
 };
 
 const LISTEN_TO_TRADEOGRE = (_, mutation) => {
   const socket = new WebSocket('wss://tradeogre.com:8080/');
 
-  socket.addEventListener('open', () => {
-    socket.send(JSON.stringify({"a":"submarket","name":"BTC-TRTL"}));
+  fetch('https://tradeogre.com/api/v1/markets')
+    .then(response => response.json())
+    .then(markets => {
+      console.log(markets);
+      const marketNames = markets.map(market => Object.keys(market)[0].split('-')[1]);
+      let marketIndex = 0;
 
-    setInterval(() => socket.send(JSON.stringify({"a":"submarket","name":"BTC-TRTL"})), 30 * 1000);
-  });
+      socket.addEventListener('open', () => {
+        const lastMarketCheckTime = new Date().getTime();
 
-  socket.addEventListener('message', ({data}) => {
-    const obj = JSON.parse(data);
+        marketNames.forEach(name => socket.send(JSON.stringify({'a': 'submarket', 'name': `BTC-${name}`})));
 
-    if (obj.a === 'orders') {
-      if (obj.t === 'buy') {
-        mutation(NEW_MARKET_DATA)('tradeogre', 'buy', 'TRTL', Object.keys(obj.d).map((price) => [stringToSatoshis(price), parseFloat(obj.d[price])]));
-      }
-      else if (obj.t === 'sell') {
-        mutation(NEW_MARKET_DATA)('tradeogre', 'sell', 'TRTL', Object.keys(obj.d).map((price) => [stringToSatoshis(price), parseFloat(obj.d[price])]));
-      }
-    }
-  });
-};
+        // socket.send(JSON.stringify({"a":"submarket","name":"BTC-TRTL"}));
 
-const LISTEN_TO_TRADESATOSHI = (_, mutation) => {
-  fetch('https://tradesatoshi.com/api/public/getorderbook?market=TRTL_BTC&type=both&depth=50')
-    .then(response => {
-      console.log({response});
+        // setInterval(() => socket.send(JSON.stringify({"a":"submarket","name":"BTC-TRTL"})), 30 * 1000);
+      });
+
+      socket.addEventListener('message', ({data}) => {
+        const obj = JSON.parse(data);
+
+        if (obj.a === 'orders') {
+          if (obj.t === 'buy') {
+            mutation(NEW_MARKET_DATA)('tradeogre', 'buy', marketNames[marketIndex], Object.keys(obj.d).map((price) => [stringToSatoshis(price), parseFloat(obj.d[price])]));
+          }
+          else if (obj.t === 'sell') {
+            mutation(NEW_MARKET_DATA)('tradeogre', 'sell', marketNames[marketIndex++], Object.keys(obj.d).map((price) => [stringToSatoshis(price), parseFloat(obj.d[price])]));
+          }
+        }
+      });
     })
-    .catch(error => {
-      console.log({error});
-    });
+    .catch(error => console.log('error fetching tradeogre markets!', error));
 };
 
 const NEW_MARKET_DATA = (_, exchange, orderType, coin, data) => {
-  const coinData = _.market[exchange][coin];
+  const coinData = (_.market[exchange][coin] = _.market[exchange][coin] || {});
   coinData[orderType] = data;
-  console.log(data.buy);
-  coinData.totalBuyMarket = (coinData.buy || []).reduce((sum, [price, amount]) => sum + amount, 0);
-  coinData.totalSellMarket = (coinData.sell || []).reduce((sum, [price, amount]) => sum + amount, 0);
-  coinData.totalMarket = coinData.totalBuyMarket + coinData.totalSellMarket;
-  console.log(_.market);
+
+  coinData.statistics = {'amount': {max: -Infinity, min: Infinity}};
+  coinData.sellStatistics = {'amount': {max: -Infinity, min: Infinity}};
+  coinData.buyStatistics = {'amount': {max: -Infinity, min: Infinity}};
+
+  coinData.totalBuyMarket = (coinData.buy || []).reduce((agg, [price, amount]) => {
+    coinData.statistics.amount.max = Math.max(coinData.statistics.amount.max, amount);
+    coinData.statistics.amount.min = Math.min(coinData.statistics.amount.min, amount);
+    coinData.buyStatistics.amount.max = Math.max(coinData.buyStatistics.amount.max, amount);
+    coinData.buyStatistics.amount.min = Math.min(coinData.buyStatistics.amount.min, amount);
+    agg.totalBTCAmount += amount * price;
+    agg.totalAmount += amount;
+    return agg;
+  }, {totalBTCAmount: 0, totalAmount: 0});
+
+  coinData.totalSellMarket = (coinData.sell || []).reduce((agg, [price, amount]) => {
+    coinData.statistics.amount.max = Math.max(coinData.statistics.amount.max, amount);
+    coinData.statistics.amount.min = Math.min(coinData.statistics.amount.min, amount);
+    coinData.sellStatistics.amount.max = Math.max(coinData.sellStatistics.amount.max, amount);
+    coinData.sellStatistics.amount.min = Math.min(coinData.sellStatistics.amount.min, amount);
+    agg.totalBTCAmount += amount * price;
+    agg.totalAmount += amount;
+    return agg;
+  }, {totalBTCAmount: 0, totalAmount: 0});
+
+  coinData.totalMarket = coinData.totalBuyMarket.totalAmount + coinData.totalSellMarket.totalAmount;
+
   return _;
 };
 
 const Markets = ({market}) => (
   <markets>
-    {console.log({market})}
-    {Object.keys(market['tradeogre']).map(coin => <Market coin={coin} data={market['tradeogre'][coin]} />)}
+    <Summary exchange={market['tradeogre']} />
+    <Charts exchange={market['tradeogre']} />
   </markets>
+);
+
+const Summary = ({exchange}) => (
+  <summary>
+    <Table headers={['Coin', 'Buy Market (btc)']} data={Object.keys(exchange)
+                       .sort((a, b) => exchange[a].totalBuyMarket.totalBTCAmount > exchange[b].totalBuyMarket.totalBTCAmount ? -1 : 1)
+                       .map(coin => [coin, (exchange[coin].totalBuyMarket.totalBTCAmount / 100000000).toFixed(8)])
+                } />
+  </summary>
+);
+
+const CoinSummary = ({coin, data}) => (
+  <coin>
+    {coin} - {(data.totalBuyMarket.totalBTCAmount / 100000000).toFixed(4)} btc
+  </coin>
+);
+
+const Table = ({data, headers}) => (
+  <table>
+    {!headers ? undefined
+              : <thead>
+                {headers.map(header => <th>{header}</th>)}
+                </thead>}
+    <tbody>
+      {data.map(row => <tr>{row.map(cell => <td>{cell}</td>)}</tr>)}
+    </tbody>
+  </table>
+);
+
+const Charts = ({exchange}) => (
+  <charts>
+    {console.log({exchange})}
+    {Object.keys(exchange)
+           .sort((a, b) => exchange[a].totalBuyMarket.totalBTCAmount > exchange[b].totalBuyMarket.totalBTCAmount ? -1 : 1)
+           .map(coin => <Market coin={coin} data={exchange[coin]} />)}
+  </charts>
 );
 
 const Market = ({coin, data}) => (
   <market>
-    <div>{coin} - {data.totalMarket}</div>
+    <div>{coin}| Total Market: ~{data.totalMarket.toFixed(0)} ||<buy>~{data.totalBuyMarket.totalAmount.toFixed(2)}</buy> to buy ({(data.totalBuyMarket.totalAmount / (data.totalBuyMarket.totalAmount + data.totalSellMarket.totalAmount)).toFixed(2)}) <sell>~{data.totalSellMarket.totalAmount.toFixed(2)}</sell> for sale  ({(data.totalSellMarket.totalAmount / (data.totalBuyMarket.totalAmount + data.totalSellMarket.totalAmount)).toFixed(2)})  [{(data.totalBuyMarket.totalAmount / data.totalSellMarket.totalAmount).toFixed(2)}]</div>
     <data>
-      <buy>{(data.buy || []).map(([price, amount]) => <PriceBar price={price} amount={amount} ofTotalMarket={amount / data.totalMarket} />)}</buy>
-      <sell>{(data.sell || []).map(([price, amount]) => <PriceBar price={price} amount={amount} ofTotalMarket={amount / data.totalMarket} />)}</sell>
+      <buy style={{'width': data.totalBuyMarket.totalAmount / (data.totalBuyMarket.totalAmount + data.totalSellMarket.totalAmount) * 100 + '%'}}>{(data.buy || []).map(([price, amount]) => <PriceBar price={price} amount={amount} height={amount / data.statistics.amount.max} />)}</buy>
+      <sell style={{'width': data.totalSellMarket.totalAmount / (data.totalBuyMarket.totalAmount + data.totalSellMarket.totalAmount) * 100 + '%'}}>{(data.sell || []).map(([price, amount]) => <PriceBar price={price} amount={amount} height={amount / data.statistics.amount.max} />)}</sell>
     </data>
   </market>
 );
 
-const PriceBar = ({price, amount, ofTotalMarket}) => (
-  <price-bar style={{'height': `${ofTotalMarket * 100}%`}} title={amount}>
-    &nbsp;
+const PriceBar = ({price, amount, height}) => (
+  <price-bar style={{'height': `${height * 100}%`}} title={amount}>
   </price-bar>
 );
 
@@ -92,11 +143,7 @@ const View = (Component) => ({started, ...props}, {mutation}) => (
 render(
   View(Markets), {
     'market': {
-      'tradeogre': {
-        'TRTL': {
-
-        }
-      }
+      'tradeogre': {}
     }
   }, document.body
 );
